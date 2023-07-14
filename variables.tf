f823ba673e304846a88cc475d60854e294ace6f8
jiayuchann <58498244+jiayuchann@users.noreply.github.com>
2023-07-14 19:35:00 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "project" {
     type = string
+    default = "bigquery-stuff-389701"
     description = "ID of GCP project"
 }
 
 variable "region" {
     type = string
+    default = "us-central1"
     description = "Region where resources will be created"
 }
