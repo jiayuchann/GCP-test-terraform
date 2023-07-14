becf709aa0601488670e284c436c1e8f0da9c751
jiayuchann <58498244+jiayuchann@users.noreply.github.com>
2023-07-14 19:29:37 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "project" {
+    type = string
+    description = "ID of GCP project"
+}
+
+variable "region" {
+    type = string
+    description = "Region where resources will be created"
+}
